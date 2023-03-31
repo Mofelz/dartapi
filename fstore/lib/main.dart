d58dca6c6275108c8d765a1f4bea9c93f313a283
Mofelz <93879842+Mofelz@users.noreply.github.com>
2023-03-31 10:59:06 +0300

    add

--- fstore/lib/main.dart
+++ fstore/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
         ),
         home: FirebaseAuth.instance.currentUser == null
             ? const EnterScreen()
